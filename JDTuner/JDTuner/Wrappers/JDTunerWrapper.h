3d9a565121b802e050133985a85f8021315b4fe9
JD-man <ekgkzmtm@gmail.com>
2026-02-06 20:04:37 +0900

    앱 프로젝트 Wrapper 및 Bridging Header 추가

---
+++ JDTuner/JDTuner/Wrappers/JDTunerWrapper.h
@@ -0,0 +1,12 @@
+//
+//  JDTunerWrapper.h
+//  JDTuner
+//
+//  Created by 조동현 on 2/6/26.
+//
+
+#ifndef JDTunerWrapper_h
+#define JDTunerWrapper_h
+
+
+#endif /* JDTunerWrapper_h */
---
+++ JDTuner/JDTuner/Wrappers/JDTunerWrapper.mm
@@ -0,0 +1,8 @@
+//
+//  JDTunerWrapper.m
+//  JDTuner
+//
+//  Created by 조동현 on 2/6/26.
+//
+
+#import <Foundation/Foundation.h>
